38961b2a6cda490666922c9818254cff8b1c8470
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-09-03 14:18:20 +0200

    add item on front end
    add item to the list only on front end

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
+import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
+import AddTodo from "./components/Addtodo";
+
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Todos,
+    Header,
+    AddTodo
   },
   data() {
     return{
@@ -36,11 +43,25 @@ export default {
   methods:{
     deleteTodo(id){
       this.todos = this.todos.filter(todo =>todo.id !== id);
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo];
     }
   }
 }
 </script>
 
 <style>
-
+  .btn{
+    display: inline-block;
+    border: none;
+    background: gray;
+    color: aliceblue;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover{
+    background: gainsboro;
+    color: gray;
+  }
 </style>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,3 +1,25 @@
 <template>
-    
+    <header class="header">
+        <h1>TodoList</h1>
+    </header>
 </template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    .header{
+        background: darkblue;
+        color: aliceblue;
+        padding: 10px;
+        text-align: center;
+    }
+
+    .header a {
+        color: aliceblue;
+        padding-right: 5px;
+    }
+</style>
