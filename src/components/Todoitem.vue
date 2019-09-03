c0f94093a8c8da17abf0793f6ba0edbad64ba3d5
GyarmatiAdam <30920398+GyarmatiAdam@users.noreply.github.com>
2019-09-03 13:33:52 +0200

    delete frontend element
    delete elements only on front end

---
+++ src/components/Todoitem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+          <input type="checkbox" v-on:change="markComplete">
+          {{todo.title}}
+          <button @click="$emit('del-todo', todo.id)" class="del">X</button>
+        </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ["todo"],
+  methods: {
+      markComplete(){
+          this.todo.completed = !this.todo.completed;
+      }
+  }
+}
+</script>
+
+<style scoped>
+.todo-item{
+    background-color: aqua;
+    padding: 10px;
+    border: 2px blue dotted;
+}
+
+.is-complete {
+    text-decoration: line-through;
+}
+.del {
+    color: red;
+    float: right;
+}
+</style>
